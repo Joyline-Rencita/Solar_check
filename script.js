94a0d982d4a438ec4b4b8c593a71346f800ce71e
MelanieCrystal <115925435+MelanieCrystal@users.noreply.github.com>
2023-04-25 18:37:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-fetch("https://www.carboninterface.com/api/v1/auth", {
-    headers: {
-        "Authorization": "Bearer 8vHiI0CHYkyzOgNzqsIVuA"
-    }
-})
-a.then((response)=>{()=> return response.json()})
-.then((response)=>{console.log(response)})
-
